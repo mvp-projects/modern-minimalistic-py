874158922f175a19bb76a24db952a2a8a0b443c3
Tomas Perez Alvarez <tomasperezalvarez@gmail.com>
2022-12-13 11:07:06 -0500

    fix RET505

--- sample/example.py
+++ sample/example.py
@@ -6,6 +6,8 @@ def some_function(first: int, second: int) -> result.Result[float, Exception]:
     """We use this function as an example for some real logic."""
     if second == 0:
         return result.Err(ZeroDivisionError())
-    elif second >= 10:
+
+    if second >= 10:
         return result.Err(ValueError("Value is to big"))
+
     return result.Ok(first / second)
