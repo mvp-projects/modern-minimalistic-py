dcdde7b1c85ae32c17fd304d0397f743c4c0ff0c
Tomas Perez Alvarez <tomasperezalvarez@gmail.com>
2022-12-13 09:52:07 -0500

    Add result library for railway pattern

--- sample/example.py
+++ sample/example.py
@@ -1,6 +1,9 @@
 """Example code."""
+from returns import result
 
 
-def some_function(first: int, second: int) -> int:
+def some_function(first: int, second: int) -> result.Result[float, Exception]:
     """We use this function as an example for some real logic."""
-    return first + second
+    if second == 0:
+        return result.Failure(ZeroDivisionError())
+    return result.Success(first / second)
--- tests/test_example.py
+++ tests/test_example.py
@@ -3,17 +3,32 @@
 import pytest
 
 from sample.example import some_function
+from returns import result
 
 
 @pytest.mark.parametrize(
     ("first", "second", "expected"),
     [
-        (1, 2, 3),
-        (2, 4, 6),
-        (-2, -3, -5),
-        (-5, 5, 0),
+        (1, 2, 0.5),
+        (2, 4, 0.5),
+        (-2, -3, 0.6666666),
+        (-5, 5, -1),
     ],
 )
 def test_some_function(first: int, second: int, expected: int) -> None:
     """Example test with parametrization."""
-    assert some_function(first, second) == expected
+    result = some_function(first, second).unwrap()
+    assert result == pytest.approx(expected)
+
+
+@pytest.mark.parametrize(
+    ("first", "second"),
+    [
+        (1, 0),
+    ],
+)
+def test_some_function_fails(first: int, second: int) -> None:
+    result = some_function(first, second)
+    err = result.failure()
+    if isinstance(err, ZeroDivisionError):
+        assert True
