70ff76692c251bfc6aa8355220f74b1e08ed3ce7
Tomas Perez Alvarez <tomasperezalvarez@gmail.com>
2022-12-13 12:19:07 -0500

    Add Hook Pre project generation

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -4,7 +4,7 @@ import sys
 
 MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 
-module_name = '{{ cookiecutter.module_name }}'
+module_name = '{{ cookiecutter.project_name }}'
 
 if not re.match(MODULE_REGEX, module_name):
     print('ERROR: %s is not a valid Python module name!' % module_name)
