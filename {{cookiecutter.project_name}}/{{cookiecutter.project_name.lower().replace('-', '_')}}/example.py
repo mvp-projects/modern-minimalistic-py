eedb5528498763cb007e877f16fd433a841a30d5
Tomas Perez Alvarez <72174660+Tomperez98@users.noreply.github.com>
2023-02-10 18:13:05 -0500

    Update example.py

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name.lower().replace('-', '_')}}/example.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name.lower().replace('-', '_')}}/example.py
@@ -4,10 +4,13 @@ import result
 
 def some_function(first: int, second: int) -> result.Result[float, Exception]:
     """We use this function as an example for some real logic."""
+    upper_bound = 10
+    
     if second == 0:
         return result.Err(ZeroDivisionError())
 
-    if second >= 10:
+    
+    if second >= upper_bound:
         return result.Err(ValueError("Value is to big"))
 
     return result.Ok(first / second)
