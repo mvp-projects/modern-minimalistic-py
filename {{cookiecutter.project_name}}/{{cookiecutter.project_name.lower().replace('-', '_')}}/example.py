5bc889c031f5f03af46d5c6027d3aa7ee26a3cca
Tomas Perez Alvarez <72174660+Tomperez98@users.noreply.github.com>
2023-03-31 11:30:58 -0500

    Update example.py (#35)

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name.lower().replace('-', '_')}}/example.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name.lower().replace('-', '_')}}/example.py
@@ -1,16 +1,16 @@
 """Example code."""
-import result
+
+from result import Err, Ok, Result
 
 
-def some_function(first: int, second: int) -> result.Result[float, Exception]:
+def some_function(first: int, second: int) -> Result[float, Exception]:
     """We use this function as an example for some real logic."""
     upper_bound = 10
-    
+
     if second == 0:
-        return result.Err(ZeroDivisionError())
+        return Err(ZeroDivisionError())
 
-    
     if second >= upper_bound:
-        return result.Err(ValueError("Value is to big"))
+        return Err(ValueError("Value is to big"))
 
-    return result.Ok(first / second)
+    return Ok(first / second)
